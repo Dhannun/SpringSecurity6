da0829998c15d505ce9679bed9fc211d4a1836c3
Dhannun <abudukhanyunus@gmail.com>
2023-04-26 08:40:35 +0100

    Added generate JWT token with and without claims function

--- src/main/java/click/touchit/security/config/JwtService.java
+++ src/main/java/click/touchit/security/config/JwtService.java
@@ -2,12 +2,20 @@ package click.touchit.security.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
+
+import static io.jsonwebtoken.SignatureAlgorithm.HS256;
 
 @Service
 public class JwtService {
@@ -15,7 +23,7 @@ public class JwtService {
   private static final String SECRET_KEY = "4528482B4D6251655468576D597133743677397A24432646294A404E635266556A586E327234753778214125442A472D4B6150645367566B5970337336763879";
 
   public String extractUsername(String token) {
-    return null;
+    return extractClaim(token, Claims::getSubject);
   }
 
   public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
@@ -23,6 +31,25 @@ public class JwtService {
     return claimsResolver.apply(claims);
   }
 
+//  JWT with claims
+  public String generateToken(
+      Map<String, Object> extraClaims,
+      UserDetails userDetails
+  ) {
+    return Jwts
+        .builder()
+        .setClaims(extraClaims)
+        .setSubject(userDetails.getUsername())
+        .setIssuedAt(new Date(System.currentTimeMillis()))
+        .setExpiration(new Date(System.currentTimeMillis() + 1_000 * 60 * 24))
+        .signWith(getSigninKey(), HS256)
+        .compact();
+  }
+
+//  JWT with no claims
+  public String generateToken(UserDetails userDetails) {
+    return generateToken(new HashMap<>(), userDetails);
+  }
   private Claims extractAllClaims(String token) {
     return Jwts
         .parserBuilder()
