97b7ad3fad1606de3012ec03fb410bcfaf1c329e
Dhannun <abudukhanyunus@gmail.com>
2023-04-26 08:47:28 +0100

    Added token validation function

--- src/main/java/click/touchit/security/config/JwtService.java
+++ src/main/java/click/touchit/security/config/JwtService.java
@@ -2,14 +2,12 @@ package click.touchit.security.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
-import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -50,6 +48,20 @@ public class JwtService {
   public String generateToken(UserDetails userDetails) {
     return generateToken(new HashMap<>(), userDetails);
   }
+
+  public boolean isTokenValid(String token, UserDetails userDetails) {
+    final String username = extractUsername(token);
+    return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+  }
+
+  private boolean isTokenExpired(String token) {
+    return extractExpiration(token).before(new Date());
+  }
+
+  private Date extractExpiration(String token) {
+    return extractClaim(token, Claims::getExpiration);
+  }
+
   private Claims extractAllClaims(String token) {
     return Jwts
         .parserBuilder()
