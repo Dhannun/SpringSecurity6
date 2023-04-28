c29d208da0ed3dc4c24b06604e91afa372342282
Dhannun <abudukhanyunus@gmail.com>
2023-04-28 01:54:18 +0100

    Added Register and Authentication Endpoints

--- src/main/java/click/touchit/security/auth/AuthenticationService.java
+++ src/main/java/click/touchit/security/auth/AuthenticationService.java
@@ -1,9 +1,17 @@
 package click.touchit.security.auth;
 
+import click.touchit.security.config.JwtService;
 import click.touchit.security.dto.request.AuthenticationRequest;
 import click.touchit.security.dto.request.RegisterRequest;
 import click.touchit.security.dto.response.AuthenticationResponse;
+import click.touchit.security.user.Role;
+import click.touchit.security.user.User;
+import click.touchit.security.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.net.URI;
@@ -12,11 +20,47 @@ import java.net.URI;
 @RequiredArgsConstructor
 public class AuthenticationService {
 
+  private final UserRepository userRepository;
+  private final PasswordEncoder passwordEncoder;
+  private final JwtService jwtService;
+  private final AuthenticationManager authenticationManager;
+
   public AuthenticationResponse register(RegisterRequest request) {
-    return null;
+
+
+    var user = User.builder()
+        .firstname(request.getFirstname())
+        .lastname(request.getLastname())
+        .email(request.getEmail())
+        .password(passwordEncoder.encode(request.getPassword()))
+        .role(Role.USER)
+        .build();
+
+    userRepository.save(user);
+
+    var jwtToken = jwtService.generateToken(user);
+
+    return AuthenticationResponse.builder()
+        .token(jwtToken)
+        .build();
   }
 
   public AuthenticationResponse authenticate(AuthenticationRequest request) {
-    return null;
+
+    authenticationManager.authenticate(
+        new UsernamePasswordAuthenticationToken(
+            request.getEmail(),
+            request.getPassword()
+        )
+    );
+
+    var user = userRepository.findByEmail(request.getEmail())
+        .orElseThrow(() -> new UsernameNotFoundException("USER_NOT_FOUND"));
+
+    var jwtToken = jwtService.generateToken(user);
+
+    return AuthenticationResponse.builder()
+        .token(jwtToken)
+        .build();
   }
 }
